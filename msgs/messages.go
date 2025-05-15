bf17fcb8d7e338c0b49759e77f7bb4a9288e0bdd
zengyu <zeng.yu.1993@gmail.com>
2025-05-15 10:08:50 +0800

    upd: implement queue status check.

--- msgs/messages.go
+++ msgs/messages.go
@@ -5,3 +5,16 @@ import "github.com/redis/go-redis/v9"
 type RedisStateMsg struct {
 	Client *redis.Client
 }
+
+type ReadgroupStatus struct {
+	LastDeliveredID string
+	Lag             int64
+	Pending         int64
+	Err             error
+}
+
+type StreamUpdateMsg struct {
+	TaskCreate  ReadgroupStatus
+	InferDown   ReadgroupStatus
+	ProcessDown ReadgroupStatus
+}
--- queue/comp.go
+++ queue/comp.go
@@ -4,27 +4,33 @@ import (
 	"context"
 	"fmt"
 	"gw/dispatcher/debugger/msgs"
+	"gw/dispatcher/debugger/style"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/redis/go-redis/v9"
 )
 
+var streamNameStyle = style.W().L
+
 const (
-	// Name of task queue.
+	// Name of task create queue.
 	taskQueueName = "task_create::stream::gw"
 
-	// Name of task queue readgroup name.
-	taskQueueReadGroupName = "task_create::readgroup::gw"
+	// Name of task inference complete queue.
+	inferCompleteQueueName = "inference_complete::stream::gw"
+
+	// Name of postprocess complete queue.
+	postprocessComplelteQueueName = "postprocess_complete::stream::gw"
 )
 
 // Check queue status period in second.
 const checkPeriod = 1
 
 type Model struct {
-	rdb              *redis.Client
-	taskQueuePending *redis.XPending
-	lastErr          error
+	rdb    *redis.Client
+	status msgs.StreamUpdateMsg
 }
 
 func New() Model {
@@ -32,11 +38,11 @@ func New() Model {
 }
 
 func (m Model) View() string {
-	if m.lastErr != nil {
-		return m.lastErr.Error()
-	}
-
-	return fmt.Sprintf("pending task count %d", m.taskQueuePending.Count)
+	return lipgloss.JoinVertical(lipgloss.Left,
+		buildCol("Task Create", &m.status.TaskCreate),
+		buildCol("Infer Down", &m.status.InferDown),
+		buildCol("Postprocess Down", &m.status.ProcessDown),
+	)
 }
 
 func (m Model) Init() tea.Cmd {
@@ -49,18 +55,12 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.rdb = msg.Client
 		return m, checkQueueStatus(m.rdb)
 
-	case checkQueueResultMessage:
-		m.lastErr = msg.Err
-		m.taskQueuePending = msg.TaskQueuePending
+	case msgs.StreamUpdateMsg:
+		m.status = msg
 		return m, delayRunCommand(checkPeriod, checkQueueStatus(m.rdb))
 	}
 
 	return m, nil
-}
-
-type checkQueueResultMessage struct {
-	TaskQueuePending *redis.XPending
-	Err              error
 }
 
 // Run command after a delay, unit is seconds.
@@ -73,7 +73,41 @@ func delayRunCommand(sec time.Duration, cmd tea.Cmd) tea.Cmd {
 
 func checkQueueStatus(rdb *redis.Client) tea.Cmd {
 	return func() tea.Msg {
-		res, err := rdb.XPending(context.Background(), taskQueueName, taskQueueReadGroupName).Result()
-		return checkQueueResultMessage{TaskQueuePending: res, Err: err}
+		result := msgs.StreamUpdateMsg{}
+		result.TaskCreate = inspectStream(rdb, taskQueueName)
+		result.InferDown = inspectStream(rdb, inferCompleteQueueName)
+		result.ProcessDown = inspectStream(rdb, postprocessComplelteQueueName)
+
+		return result
 	}
+}
+
+func inspectStream(rdb *redis.Client, key string) msgs.ReadgroupStatus {
+	info, err := rdb.XInfoGroups(context.Background(), key).Result()
+	if err != nil {
+		return msgs.ReadgroupStatus{Err: err}
+	}
+
+	// It always one read group bucause all consumer use same group name.
+	// Get last delivered id then read messages after that.
+	result := msgs.ReadgroupStatus{Err: nil}
+	result.LastDeliveredID = info[0].LastDeliveredID
+	result.Lag = info[0].Lag
+	result.Pending = info[0].Pending
+
+	return result
+}
+
+func buildCol(title string, data *msgs.ReadgroupStatus) string {
+	var header, info string
+
+	header = streamNameStyle.Render(title + ":")
+
+	if data.Err != nil {
+		info = data.Err.Error()
+	} else {
+		info = fmt.Sprintf("%d waiting, %d processing", data.Lag, data.Pending)
+	}
+
+	return lipgloss.JoinHorizontal(lipgloss.Bottom, header, info)
 }
